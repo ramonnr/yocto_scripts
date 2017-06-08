49dd3a4fbc72214dfbde4c030aac859600fa0c82
RNR <ramon.nordin.rodriguez@avaloninnovation.com>
2017-06-08 14:54:49 +0200

    added check for command line arguments

--- boot_rootfs/boot_clean_cpy.sh
+++ boot_rootfs/boot_clean_cpy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #startarg build: buildfolder name in yocto, default is build
 
+if [ $# -ne 1 ];then
+  echo Need builddir name as argument
+  exit 1
+fi
+
 build=$1
 echo clean and copy from buildmachine to sdcard 
 rm ~/boot/*
-scp user@buildmachine:/home/rodz/git/poky/$build/tmp/deploy/images/beaglebone/zImage ~/boot/
-scp user@buildmachine:/home/rodz/git/poky/$build/tmp/deploy/images/beaglebone/MLO ~/boot/
-scp user@buildmachine:/home/rodz/git/poky/$build/tmp/deploy/images/beaglebone/u-boot.img ~/boot/
-scp user@buildmachine:/home/rodz/git/poky/$build/tmp/deploy/images/beaglebone/zImage-am335x-boneblack.dtb ~/boot/
+scp user@buildmachine:/home/username/git/poky/$build/tmp/deploy/images/beaglebone/zImage ~/boot/
+scp user@buildmachine:/home/username/git/poky/$build/tmp/deploy/images/beaglebone/MLO ~/boot/
+scp user@buildmachine:/home/username/git/poky/$build/tmp/deploy/images/beaglebone/u-boot.img ~/boot/
+scp user@buildmachine:/home/username/git/poky/$build/tmp/deploy/images/beaglebone/zImage-am335x-boneblack.dtb ~/boot/
 ls -al ~/boot/
 rm /media/$USER/boot/*
 cd /media/$USER/boot
--- boot_rootfs/roots_clean_cpy.sh
+++ boot_rootfs/roots_clean_cpy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+#startarg build: buildfolder name in yocto, default is build
+
+if [ $# -ne 1 ];then
+  echo Need builddir name as argument
+  exit 1
+fi
+
 build=$1
 
 echo clean and copy from buidmachine to sdcard 
 rm ~/rootfs/*
-scp user@buildmachine:/home/rodz/git/poky/$build/tmp/deploy/images/beaglebone/*.bz2 ~/rootfs/
+scp user@buildmachine:/home/username/git/poky/$build/tmp/deploy/images/beaglebone/*.bz2 ~/rootfs/
 ls -al ~/rootfs/
-sudo rm -rf /media/$USER/root/*
-cd /media/$USER/root
+sudo rm -rf /media/$USER/rootfs/*
+cd /media/$USER/rootfs
 sudo tar -xf ~/rootfs/*.rootfs*.bz2 .
-ls -al /media/$USER/root
+ls -al /media/$USER/rootfs
 echo script complete
 
